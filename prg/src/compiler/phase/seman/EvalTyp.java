bbc721a1d391ecd45b3f8df9a5d9142600c12101
Jaka Vute <jv5542@student.uni-lj.si>
2016-04-18 15:11:14 +0200

    started work on the type evaluation

--- prg/src/compiler/phase/seman/EvalTyp.java
+++ prg/src/compiler/phase/seman/EvalTyp.java
@@ -30,5 +30,160 @@ public class EvalTyp extends FullVisitor {
 	/** The symbol table. */
 	private SymbolTable symbolTable = new SymbolTable();
 
-	// TODO
+	public void visit(ArrType arrType) {
+		arrType.size.accept(this);
+		arrType.elemType.accept(this);
+	}
+
+	public void visit(AtomExpr atomExpr) {
+	    switch(atomExpr.type){
+            case INTEGER:
+                attrs.typAttr.set(atomExpr,new IntegerTyp());
+                break;
+            case BOOLEAN:
+                attrs.typAttr.set(atomExpr,new BooleanTyp());
+                break;
+            case CHAR:
+                attrs.typAttr.set(atomExpr,new CharTyp());
+                break;
+            case STRING:
+                attrs.typAttr.set(atomExpr,new StringTyp());
+                break;
+            case VOID:
+                attrs.typAttr.set(atomExpr,new VoidTyp());
+                break;
+            case PTR:
+                attrs.typAttr.set(atomExpr,new PtrTyp(new VoidTyp()));
+                break;
+        }
+	}
+
+	public void visit(AtomType atomType) {
+	    switch(atomType.type){
+            case INTEGER:
+                attrs.typAttr.set(atomType,new IntegerTyp());
+                break;
+            case BOOLEAN:
+                attrs.typAttr.set(atomType,new BooleanTyp());
+                break;
+            case CHAR:
+                attrs.typAttr.set(atomType,new CharTyp());
+                break;
+            case STRING:
+                attrs.typAttr.set(atomType,new StringTyp());
+                break;
+            case VOID:
+                attrs.typAttr.set(atomType,new VoidTyp());
+                break;
+        }
+	}
+
+	public void visit(BinExpr binExpr) {
+		binExpr.fstExpr.accept(this);
+		binExpr.sndExpr.accept(this);
+	}
+
+	public void visit(CastExpr castExpr) {
+		castExpr.type.accept(this);
+		castExpr.expr.accept(this);
+	}
+
+	public void visit(CompDecl compDecl) {
+		compDecl.type.accept(this);
+	}
+
+	public void visit(CompName compName) {
+	}
+	
+	public void visit(DeclError declError) {
+	}
+
+	public void visit(Exprs exprs) {
+		for (int e = 0; e < exprs.numExprs(); e++)
+			exprs.expr(e).accept(this);
+	}
+
+	public void visit(ExprError exprError) {
+	}
+
+	public void visit(ForExpr forExpr) {
+		forExpr.var.accept(this);
+		forExpr.loBound.accept(this);
+		forExpr.hiBound.accept(this);
+		forExpr.body.accept(this);
+	}
+
+	public void visit(FunCall funCall) {
+		for (int a = 0; a < funCall.numArgs(); a++)
+			funCall.arg(a).accept(this);
+	}
+
+	public void visit(FunDecl funDecl) {
+		for (int p = 0; p < funDecl.numPars(); p++)
+			funDecl.par(p).accept(this);
+		funDecl.type.accept(this);
+	}
+
+	public void visit(FunDef funDef) {
+		for (int p = 0; p < funDef.numPars(); p++)
+			funDef.par(p).accept(this);
+		funDef.type.accept(this);
+		funDef.body.accept(this);
+	}
+
+	public void visit(IfExpr ifExpr) {
+		ifExpr.cond.accept(this);
+		ifExpr.thenExpr.accept(this);
+		ifExpr.elseExpr.accept(this);
+	}
+
+	public void visit(ParDecl parDecl) {
+		parDecl.type.accept(this);
+	}
+
+	public void visit(Program program) {
+		program.expr.accept(this);
+	}
+
+	public void visit(PtrType ptrType) {
+		ptrType.baseType.accept(this);
+	}
+
+	public void visit(RecType recType) {
+		for (int c = 0; c < recType.numComps(); c++)
+			recType.comp(c).accept(this);
+	}
+
+	public void visit(TypeDecl typDecl) {
+		typDecl.type.accept(this);
+	}
+	
+	public void visit(TypeError typeError) {
+	}
+
+	public void visit(TypeName typeName) {
+	}
+
+	public void visit(UnExpr unExpr) {
+		unExpr.subExpr.accept(this);
+	}
+
+	public void visit(VarDecl varDecl) {
+		varDecl.type.accept(this);
+	}
+
+	public void visit(VarName varName) {
+	}
+
+	public void visit(WhereExpr whereExpr) {
+		whereExpr.expr.accept(this);
+		for (int d = 0; d < whereExpr.numDecls(); d++)
+			whereExpr.decl(d).accept(this);
+	}
+
+	public void visit(WhileExpr whileExpr) {
+		whileExpr.cond.accept(this);
+		whileExpr.body.accept(this);
+	}
+
 }
