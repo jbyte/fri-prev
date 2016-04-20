d6a1469b64a20f205718e285e53aff7fda5d0b3d
Jaka Vute <jv5542@student.uni-lj.si>
2016-04-20 14:39:40 +0200

    removed a double paste error

--- prg/src/compiler/data/typ/BooleanTyp.java
+++ prg/src/compiler/data/typ/BooleanTyp.java
@@ -20,11 +20,6 @@ public class BooleanTyp extends AtomTyp implements AssignableTyp, ComparableTyp,
     }
  
     @Override
-    public long size() {
-        return 1;
-    }
- 
-    @Override
     public void log(Logger logger) {
         logger.begElement("typ");
         logger.addAttribute("kind", "BOOLEAN");
