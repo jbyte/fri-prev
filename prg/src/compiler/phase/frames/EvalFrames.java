ae2144ff9bd8f792e54a58e2df24f8e9f4bf29e8
Jaka Vute <jv5542@student.uni-lj.si>
2016-04-23 12:14:57 +0200

    offset access try no. 1

--- prg/src/compiler/phase/frames/EvalFrames.java
+++ prg/src/compiler/phase/frames/EvalFrames.java
@@ -24,17 +24,38 @@ public class EvalFrames extends FullVisitor {
 
     @Override
     public void visit(FunDecl funDecl) {
-        for (int p = 0; p < funDecl.numPars(); p++)
+        long offset = 0;
+        for (int p = 0; p < funDecl.numPars(); p++){
             funDecl.par(p).accept(this);
+            Typ typ = attrs.typAttr.get(funDecl.par(p));
+            attrs.accAttr.set(funDecl.par(p),new OffsetAccess(offset,typ.size()));
+            offset += typ.size();
+        }
         funDecl.type.accept(this);
     }
 
     @Override
     public void visit(FunDef funDef) {
-        for (int p = 0; p < funDef.numPars(); p++)
+        long offset = 0;
+        for (int p = 0; p < funDef.numPars(); p++){
             funDef.par(p).accept(this);
+            Typ typ = attrs.typAttr.get(funDef.par(p));
+            attrs.accAttr.set(funDef.par(p),new OffsetAccess(offset,typ.size()));
+            offset += typ.size();
+        }
         funDef.type.accept(this);
         funDef.body.accept(this);
+    }
+
+    @Override
+    public void visit(RecType recType) {
+        long offset = 0;
+        for (int c = 0; c < recType.numComps(); c++){
+            recType.comp(c).accept(this);
+            Typ typ = attrs.typAttr.get(recType.comp(c));
+            attrs.accAttr.set(recType.comp(c),new OffsetAccess(offset,typ.size()));
+            offset += typ.size();
+        }
     }
 
     @Override
